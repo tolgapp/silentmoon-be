db0d3e6937fb07a8ac120fe910416e57e83e5839
Tolga <123948207+tolgapp@users.noreply.github.com>
2025-01-26 10:24:05 +0100

    added vercel json

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,33 @@ import path from "path";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import routing from "./routes/routing";
-import spotifyRoutes from "./routes/spotifyRoutes"
-import cookieParser from 'cookie-parser';
+import spotifyRoutes from "./routes/spotifyRoutes";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5002;
 const allowedOrigins = process.env.FRONTEND_LINK;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', "userid"],
-  optionsSuccessStatus: 204
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "userid"],
+    optionsSuccessStatus: 204,
+  })
+);
 
-app.use('/images', express.static(path.join(__dirname, '../public/images')));
-app.use('/api/videos', express.static(path.join(__dirname, '../public/videos')));
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+app.use(
+  "/api/videos",
+  express.static(path.join(__dirname, "../public/videos"))
+);
 
 mongoose
   .connect(process.env.MONGO_URI as string)
@@ -32,7 +37,7 @@ mongoose
   .catch((error) => console.log("MongoDB connection error", error));
 
 app.use("/api", routing);
-app.use("/api", spotifyRoutes)
+app.use("/api", spotifyRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
