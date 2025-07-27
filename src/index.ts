d8b0d8350a5b40348c0e07831db013d7783effb8
Tolga <123948207+tolgapp@users.noreply.github.com>
2025-07-27 11:40:23 +0200

    refactored index.ts and vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import routing from './routes/routing';
-import spotifyRoutes from './routes/spotifyRoutes';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import routing from "./routes/routing";
+import spotifyRoutes from "./routes/spotifyRoutes";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
@@ -16,39 +15,32 @@ const allowedOrigins = process.env.FRONTEND_LINK;
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use(
   cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'userid'],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "userid"],
     optionsSuccessStatus: 204,
   })
 );
 
-app.use('/images', express.static(path.join(__dirname, '../public/images')));
-app.use('/api/videos', express.static(path.join(__dirname, '../public/videos')));
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+app.use(
+  "/api/videos",
+  express.static(path.join(__dirname, "../public/videos"))
+);
 
-app.use('/api', routing);
-app.use('/api', spotifyRoutes);
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log("MongoDB connection error", error));
 
-let isConnected = false;
-async function connectDB() {
-  if (isConnected) return;
-  await mongoose.connect(process.env.MONGO_URI as string);
-  isConnected = true;
-}
+app.use("/api", routing);
+app.use("/api", spotifyRoutes);
 
-if (require.main === module) {
-  connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log(`✅ Running @ http://localhost:${PORT}`);
-    });
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await connectDB();
-  return app(req, res);
-}
+export default app;
